6be123bee1cc8e6f5045e8ac0be558cec6d0a978
jungwooleecode <jung.lee@code.berlin>
2022-08-06 13:41:51 +0900

    transform new file

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from kivy.core.window import Window
 from kivy import platform
 
 class MainWidget(Widget):
+    from transforms import transform, transform_2D, transform_perspective
+
     perspective_point_x= NumericProperty(0)
     perspective_point_y= NumericProperty(0)
 
@@ -109,29 +111,6 @@ class MainWidget(Widget):
             x1, y1= self.transform( xmin, line_y)
             x2, y2= self.transform( xmax, line_y)
             self.horizontal_lines[i].points=[x1, y1, x2, y2]
-    
-
-    def transform(self, x, y):
-        return self.transform_perspective(x,y)
-        #return self.transform_2D(x,y)
-    
-    def transform_2D(self, x, y):
-        return int(x), int(y)
-
-    def transform_perspective(self, x, y):
-        tr_y= y* self.perspective_point_y/ self.height
-        if tr_y> self.perspective_point_y:
-            tr_y= self.perspective_point_y
-
-        diff_x= x- self.perspective_point_x
-        diff_y= self.perspective_point_y- tr_y
-        factor_y= diff_y/self.perspective_point_y
-        factor_y= pow(factor_y, 2) # factor_y power of 2
-
-        tr_x=self.perspective_point_x + diff_x*factor_y
-        tr_y= (1 - factor_y)* self.perspective_point_y
-
-        return int(tr_x), int(tr_y)
 
     def on_keyboard_down(self, keyboard, keycode, text, modifiers):
         if keycode[1] == 'left':
