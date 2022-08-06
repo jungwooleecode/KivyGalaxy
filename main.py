14f3f9fd1746e26037bb93ad251571e74d161283
jungwooleecode <jung.lee@code.berlin>
2022-08-06 12:22:44 +0900

    layout moving keypress done

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from platform import platform
+from kivy.config import Config
+Config.set('graphics', 'width', '900')
+Config.set('graphics', 'height', '400')
+
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, Clock
 from kivy.graphics.context_instructions import Color
 from kivy.graphics.vertex_instructions import Line
+from kivy.core.window import Window
+from kivy import platform
 
 class MainWidget(Widget):
     perspective_point_x= NumericProperty(0)
@@ -16,17 +23,42 @@ class MainWidget(Widget):
     H_LINES_SPACING= 0.1 #percentage in screen length
     horizontal_lines=[]
 
+    SPEED=4
+    SPEED_X=12
+
+    current_speed_x=0
+
+    current_offset_y=0
+    current_offset_x=0
+
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         self.init_vertical_lines()
         self.init_horizontal_lines()
+        
+        if self.is_desktop():
+            self._keyboard = Window.request_keyboard(self.keyboard_closed, self)
+            self._keyboard.bind(on_key_down=self.on_keyboard_down)
+            self._keyboard.bind(on_key_up=self.on_keyboard_up)
+
+        Clock.schedule_interval(self.update, 1/60)
+
+    def keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self.on_keyboard_down)
+        self._keyboard.unbind(on_key_up=self.on_keyboard_up)
+        self._keyboard = None
+
+    def is_desktop(self):
+        if platform in ('linux', 'windows', 'macosx'):
+            return True
+
+        return False
 
     def on_parent(self, widget, parent):
         pass
     
     def on_size(self, *args):
-        self.update_vertical_lines()
-        self.update_horizontal_lines()
+        pass
 
     def on_perspective_point_x(self, widget, value):
         pass
@@ -47,7 +79,7 @@ class MainWidget(Widget):
         spacing= self.V_LINES_SPACING*self.width
 
         for i in range(0, self.V_NB_LINES):
-            line_x= int(central_line_x + offset*spacing)
+            line_x= central_line_x + offset*spacing + self.current_offset_x
 
             x1, y1= self.transform(line_x, 0)
             x2, y2= self.transform(line_x, self.height)
@@ -66,13 +98,14 @@ class MainWidget(Widget):
         offset= -int(self.V_NB_LINES/2)+0.5
         spacing= self.V_LINES_SPACING*self.width
         
-        xmin= central_line_x + offset*spacing
-        xmax= central_line_x - offset*spacing
+        xmin= central_line_x + offset*spacing + self.current_offset_x
+        xmax= central_line_x - offset*spacing + self.current_offset_x
+
         spacing_y=self.H_LINES_SPACING*self.height
 
         for i in range(0, self.H_NB_LINES):
-            line_y= 0 +i*spacing_y
 
+            line_y= 0 +i*spacing_y- self.current_offset_y
             x1, y1= self.transform( xmin, line_y)
             x2, y2= self.transform( xmax, line_y)
             self.horizontal_lines[i].points=[x1, y1, x2, y2]
@@ -99,6 +132,45 @@ class MainWidget(Widget):
         tr_y= (1 - factor_y)* self.perspective_point_y
 
         return int(tr_x), int(tr_y)
+
+    def on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == 'left':
+            self.current_speed_x= self.SPEED_X
+        elif keycode[1] == 'right':
+            self.current_speed_x= -self.SPEED_X
+        
+        return True
+
+    def on_keyboard_up(self, keyboard, keycode):
+        self.current_speed_x=0
+        return True
+    
+    def on_touch_down(self, touch):
+        if touch.x <self.width/2:
+           # print("<-")
+            self.current_speed_x= self.SPEED_X
+        else:
+           # print("->")
+            self.current_speed_x= -self.SPEED_X
+
+    def on_touch_up(self, touch):
+        print("UP")    
+        self.current_speed_x=0
+    
+    def update(self, dt):
+
+        time_factor = dt*60
+        self.update_vertical_lines()
+        self.update_horizontal_lines()
+
+        self.current_offset_y += self.SPEED*time_factor
+
+        spacing_y=self.H_LINES_SPACING*self.height
+
+        if self.current_offset_y >= spacing_y:
+            self.current_offset_y -= spacing_y
+
+        self.current_offset_x += self.current_speed_x *time_factor
 
 class GalaxyApp(App):
     pass
