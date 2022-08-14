4fe954f705dfef7a38abf81f7db30fc9afec0c1a
jungwooleecode <jung.lee@code.berlin>
2022-08-14 11:25:11 +0900

    real final

--- user_actions.py
+++ user_actions.py
@@ -1,3 +1,6 @@
+#from .main import MainWidget
+from kivy.uix.relativelayout import RelativeLayout
+
 def keyboard_closed(self):
     self._keyboard.unbind(on_key_down=self.on_keyboard_down)
     self._keyboard.unbind(on_key_up=self.on_keyboard_up)
@@ -16,12 +19,16 @@ def on_keyboard_up(self, keyboard, keycode):
     return True
     
 def on_touch_down(self, touch):
-    if touch.x <self.width/2:
-        # print("<-")
-        self.current_speed_x= self.SPEED_X
-    else:
-        # print("->")
-        self.current_speed_x= -self.SPEED_X
+
+    if not self.state_game_over and self.state_game_has_started:
+        if touch.x <self.width/2:
+            # print("<-")
+            self.current_speed_x= self.SPEED_X
+        else:
+            # print("->")
+            self.current_speed_x= -self.SPEED_X
+
+    return super(RelativeLayout, self).on_touch_down(touch)
 
 def on_touch_up(self, touch):
     print("UP")    
